69365804faae82eb52069c771679e7b5dc4e3591
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-05-22 14:54:36 -0600

    commit of package json

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const data = require('./instructors.js');
 const app = express();
 const port = process.env.port || 5000;
 
-app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '/public'));
 app.use(cors());
 
 app.get('/', (req, res) => {

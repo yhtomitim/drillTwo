77c596e973182cb3acef8fd736e59a085e904c34
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-05-22 13:51:31 -0600

    add basic GET route

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.port || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send({ message: 'hello world' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});

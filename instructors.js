9f90d5da38e8f8f5692fa1655d6f580a07f8b878
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-05-22 14:34:10 -0600

    add JSON data

---
+++ instructors.js
@@ -0,0 +1,76 @@
+module.exports = {
+  data: [
+    {
+      'id': 1,
+      'fullName': 'Kyle Coberly',
+      'title': 'Faculty Director',
+      'numberOfDogs': 0
+    },
+    {
+      'id': 2,
+      'fullName': 'Danny Fritz',
+      'title': 'Lead Instructor',
+      'numberOfDogs': 0
+    },
+    {
+      'id': 3,
+      'fullName': 'CJ Reynolds',
+      'title': 'Lead Instructor',
+      'numberOfDogs': 0
+    },
+    {
+      'id': 4,
+      'fullName': 'Brooks Patton',
+      'title': 'Lead Instructor',
+      'numberOfDogs': 0
+    },
+    {
+      'id': 5,
+      'fullName': 'Roberto Ortega',
+      'title': 'Lead Instructor',
+      'numberOfDogs': 1
+    },
+    {
+      'id': 6,
+      'fullName': 'Chad Drummond',
+      'title': 'Instructor',
+      'numberOfDogs': 0
+    },
+    {
+      'id': 7,
+      'fullName': 'Kim Schlesinger',
+      'title': 'Instructor',
+      'numberOfDogs': 0
+    },
+    {
+      'id': 8,
+      'fullName': 'Peter Ostiguy',
+      'title': 'Associate Instructor',
+      'numberOfDogs': 1
+    },
+    {
+      'id': 9,
+      'fullName': 'Cass Torske',
+      'title': 'Resident',
+      'numberOfDogs': 1
+    },
+    {
+      'id': 10,
+      'fullName': 'Matt Winzer',
+      'title': 'Resident',
+      'numberOfDogs': 2
+    },
+    {
+      'id': 11,
+      'fullName': 'Aaron Goodman',
+      'title': 'Resident',
+      'numberOfDogs': 0
+    },
+    {
+      'id': 12,
+      'fullName': 'Michelle Bergquist',
+      'title': 'Resident',
+      'numberOfDogs': 1
+    }
+  ]
+};
